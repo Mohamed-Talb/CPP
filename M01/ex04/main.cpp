c39449d36242a2ae51245856ded46d7b13433e64
Mohamed  Taleb <mtaleb@c2r4p14.1337.ma>
2025-12-17 14:02:10 +0100

    pushing exercices: M01/ex01/ex01/ex02/ex03/ex04

--- M01/ex04/main.cpp
+++ M01/ex04/main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <fstream>
 
-// void replace(std::string content, std::string s1, std::string s2)
+// std::string replace(std::string content, const std::string& s1, const std::string& s2)
 // {
-//     for
+//     std::string newcontent;
+//     size_t index;
+//     while ((index = content.find(s1)) != std::string::npos)
+//     {
+//         newcontent += content.substr(0, index);
+//         newcontent += s2;
+//         content = content.substr(index + s1.length());
+//     }
+//     newcontent += content;
+//     return newcontent;
 // }
+
+std::string replace(std::string content, const std::string& s1, const std::string& s2)
+{
+    size_t index;
+    size_t s1len = s1.length();
+    while ((index = content.find(s1)) != std::string::npos)
+    {
+        content.erase(index, s1len);
+        content.insert(index, s2);
+    }
+    return content;
+}
+
 
 int main(int ac, char **av)
 {
-    std::string s1;
-    std::string s2;
+    std::string s1 = av[2];
+    std::string s2 = av[3];
     std::string file = av[1];
     std::string content;
     std::string Buffer;
@@ -20,5 +42,10 @@ int main(int ac, char **av)
             Buffer += '\n';
         content += Buffer;
     }
-    std::cout << content << std::endl;
+    intfile.close();
+    std::ofstream outfile("hello.txt");
+    std::string nec = replace(content, s1, s2);
+    // outfile << replace(content, s1, s2);
+    outfile << nec;
+    std::cout << nec;
 }
