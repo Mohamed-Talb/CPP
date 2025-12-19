1926f25e05141712d043d32e705235d16d6fae8e
Mohamed  Taleb <mtaleb@c2r4p14.1337.ma>
2025-12-19 19:40:00 +0100

    add exercices: M01/ex03 ex04 ex05

--- M01/ex00/main.cpp
+++ M01/ex00/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::string name = "Walker1";
     Zombie *newZ = newZombie(name);
-    randomChump("Tyler2");
-    delete newZ;
+    newZ->announce();
+    randomChump("Tyler2"); // malloc and new with obj placment new 
+    delete newZ; // delete with distructor 
 }
---
+++ M01/ex05/Harl.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Harl
+{
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+    public:
+        void complain(std::string level);
+};
