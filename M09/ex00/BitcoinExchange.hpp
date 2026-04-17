583995151b13ddfbda33d02c4ca442567a41eac9
mtaleb <mtaleb.contact@gmail.com>
2026-04-17 19:28:12 +0100

    ADD M09/ex00

---
+++ M09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream> 
+#include <fstream>
+#include <map>
+
+class BitcoinExchange
+{
+	
+};
+
+#endif
