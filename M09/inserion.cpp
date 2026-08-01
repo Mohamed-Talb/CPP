e08ec44339c88ea05331e2edb53a6e58d46bea8e
Mohamed  Taleb <mtaleb.contact@gmail.com>
2026-08-01 19:58:14 +0100

    feat: add initial recursive Ford-Johnson sorting implementation

--- M09/inserion.cpp
+++ M09/inserion.cpp
@@ -4,41 +4,142 @@
 #include <random>
 #include <algorithm>
 
-std::vector<int> randomArr(size_t size)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
+std::vector<int> Numbers;
+typedef std::vector<std::pair<int, int> > Pairs_t;
 
-    std::uniform_int_distribution<> dist(0, 100);
-    std::vector<int> rVector;
-    for (size_t i = 0; i < size; i++)
+void AC6_insertLeft(std::vector<int>  &SortedNumbers, int &left)
+{
+    std::vector<int>::iterator isPos = std::lower_bound(SortedNumbers.begin(), SortedNumbers.end(), left);
+    SortedNumbers.insert(isPos, left);
+    // for (int i = 0; i < SortedNumbers.size(); i++)
+    // {
+    //     if ()
+    // }
+
+}
+
+std::vector<int>  AC5_insertLoosers(Pairs_t &sortedPairs, std::vector<int> &sortedWinners)
+{
+    if (sortedPairs.empty())
+        return sortedWinners;
+    std::vector<int> sortedNumbers = sortedWinners;
+    sortedNumbers.insert(sortedNumbers.begin(), sortedPairs[0].first);
+
+    for (int i = 1; i < sortedPairs.size(); i++)
     {
-        rVector.push_back(dist(gen));
+        int looser = sortedPairs[i].first;
+        int winner = sortedPairs[i].second;
+
+        std::vector<int>::iterator winnerPos = find(sortedNumbers.begin(), sortedNumbers.end(), winner);
+        std::vector<int>::iterator insertionPos = std::lower_bound(sortedNumbers.begin(), winnerPos, looser);
+        sortedNumbers.insert(insertionPos, looser);
     }
-    return rVector;
+    return sortedNumbers;
+}
+
+Pairs_t AC4_SortedPairsByWinners(Pairs_t &Pairs, std::vector<int> &sortedWinners)
+{
+    Pairs_t sortedPairs;
+    for(int i = 0; i < sortedWinners.size(); i++)
+    {
+        for(int j = 0; j < Pairs.size(); j++)
+        {
+            if (Pairs[j].second == sortedWinners[i])
+            {
+                sortedPairs.push_back(Pairs[j]);
+                break;
+            }
+        }
+    }
+    return sortedPairs;
+}
+
+void AC3_GetWinners(Pairs_t &Pairs, std::vector<int> &Winners)
+{
+    for(int i = 0; i < Pairs.size(); i++)
+    {
+        Winners.push_back(Pairs[i].second);
+    }
+}
+
+void AC2_SortEachPair(Pairs_t &Pairs)
+{
+    for (int i = 0; i < Pairs.size(); i++)
+    {
+        if (Pairs[i].first > Pairs[i].second)
+            std::swap(Pairs[i].first, Pairs[i].second);
+    }
+}
+
+Pairs_t AC1_CreatePairs(std::vector<int> Sequence, int &leftOver)
+{
+    Pairs_t pairs;
+    if (Sequence.size() % 2 != 0)
+    {
+        leftOver = Sequence.back();
+        Sequence.pop_back();
+    }
+    for(int i = 0; i < Sequence.size(); i+=2)
+    {
+        pairs.push_back(std::make_pair(Sequence[i], Sequence[i + 1]));
+    }
+    return pairs;
 }
 
 
+std::vector<int> FordJhonson(std::vector<int> Sequence)
+{
+    if (Sequence.size() <= 1)
+    {
+        return Sequence;
+    }
+    int leftOver = -1;
+    std::vector<int> Winners;
+    Pairs_t Pairs = AC1_CreatePairs(Sequence, leftOver);
+    AC2_SortEachPair(Pairs);
+    AC3_GetWinners(Pairs, Winners);
+    std::vector<int> sortedWinners = FordJhonson(Winners);
+    Pairs_t sortedPairs = AC4_SortedPairsByWinners(Pairs, sortedWinners);
+    std::vector<int> sortedNumbers = AC5_insertLoosers(sortedPairs, sortedWinners);
+    if (leftOver != -1)
+        AC6_insertLeft(sortedNumbers, leftOver);
+    return sortedNumbers;
+}
+
+
+#include <iostream>
+#include <vector>
+
+static void printVector(const std::vector<int> &sequence)
+{
+    for (std::vector<int>::size_type i = 0; i < sequence.size(); ++i)
+    {
+        std::cout << sequence[i];
+
+        if (i + 1 < sequence.size())
+            std::cout << " ";
+    }
+
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::vector<int> RV = randomArr(10);
-    for (int x : RV)
-        std::cout << x << " ";
-    std::cout << std::endl;
-    int tmp;
-    // INSERTION SORT 
+    std::vector<int> sequence;
 
-    for (size_t i = 1; i < RV.size(); i++)
-    {
-        for (size_t j = i; j > 0; j--)
-        {
-            if (RV[j] < RV[j - 1])
-                std::swap(RV[j], RV[j - 1]);
-            else
-                break;
-        }
-    }
-    for (int x : RV)
-        std::cout << x << " ";
+    sequence.push_back(10);
+    sequence.push_back(3);
+    sequence.push_back(7);
+    sequence.push_back(2);
+    sequence.push_back(8);
+
+    std::cout << "Before: ";
+    printVector(sequence);
+
+    std::vector<int> sortedSequence = FordJhonson(sequence);
+
+    std::cout << "After:  ";
+    printVector(sortedSequence);
+
+    return 0;
 }
